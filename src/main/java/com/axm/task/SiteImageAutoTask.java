b7d2b2b45f810a224a7a2d3222e81f946f9935eb
AceXiamo <xiamo@qwq.link>
2023-08-02 15:24:21 +0800

    chore: code format

--- src/main/java/com/axm/task/SiteImageAutoTask.java
+++ src/main/java/com/axm/task/SiteImageAutoTask.java
@@ -6,7 +6,6 @@ import com.axm.service.SitePreviewService;
 import com.axm.utils.PreviewUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,7 +18,6 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
-@EnableScheduling
 public class SiteImageAutoTask {
 
     @Autowired
